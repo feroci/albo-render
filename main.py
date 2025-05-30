8c965ce9da66e8d2472c34c4d30d6d94e8c1ab79
feroci <73292493+feroci@users.noreply.github.com>
2025-05-30 08:46:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument("--headless")  # anche se c'è Xvfb, meglio tenerlo
 
 driver = webdriver.Firefox(options=options)
-driver.get("https://www.example.com")
-print("Titolo della pagina:", driver.title)
+driver.get("https://example.com")
+print(driver.title)
 driver.quit()
